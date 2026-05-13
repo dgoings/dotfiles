1661a042c16a6fd96eca82970f1c98c1d958d587
Dylan Goings <dylan@dgoings.com>
2026-05-13 13:21:50 -0400

    Add Android Studio java to shell path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
   fish_vi_key_bindings
+  bind -M insert \e\x7f backward-kill-word
 end
 export PATH="$HOME/.local/bin:$PATH"
 
@@ -8,4 +9,17 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 # nvm
-set --universal nvm_default_version v18.4.0
+set --universal nvm_default_version 25
+# pnpm
+set -gx PNPM_HOME "/Users/dylan.goings/Library/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
+
+# Java
+echo 'set -gx JAVA_HOME "/Applications/Android Studio.app/Contents/jbr/Contents/Home"' >> ~/.config/fish/config.fish
+fish_add_path $JAVA_HOME/bin
+set -gx JAVA_HOME "/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+set -gx JAVA_HOME "/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+set -gx JAVA_HOME "/Applications/Android Studio.app/Contents/jbr/Contents/Home"
