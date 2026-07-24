b3e1121525ae6c2f037cc80e424815803429a589
Dylan Goings <dylan@dgoings.com>
2026-07-24 14:49:03 -0400

    Sync config changes from new laptop

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -22,3 +22,6 @@ if test -d $jbr
   set -gx JAVA_HOME $jbr
   fish_add_path -g $JAVA_HOME/bin
 end
+set -gx ANDROID_HOME "$HOME/Library/Android/sdk"
+set -gx ANDROID_SDK_ROOT $ANDROID_HOME
+fish_add_path $ANDROID_HOME/platform-tools $ANDROID_HOME/emulator $ANDROID_HOME/cmdline-tools/latest/bin
